33470d340d239c60fca5f4bd0c66c77ddf6b0afe
Vadim Zalunin <vadim.zalunin@gmail.com>
2015-06-05 09:49:28 +0100

    fixed crai indexer: version aware reading

--- src/main/java/net/sf/cram/index/CraiIndexer.java
+++ src/main/java/net/sf/cram/index/CraiIndexer.java
@@ -16,7 +16,6 @@
 package net.sf.cram.index;
 
 import java.io.BufferedOutputStream;
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -29,20 +28,18 @@ import net.sf.cram.io.CountingInputStream;
 import net.sf.cram.structure.Container;
 import net.sf.cram.structure.CramHeader;
 import net.sf.picard.util.Log;
-import net.sf.samtools.SAMFileHeader;
 
 class CraiIndexer {
 	private static Log log = Log.getInstance(CraiIndexer.class);
 
 	private CountingInputStream is;
-	private SAMFileHeader samFileHeader;
 	private CramIndex index;
+	private CramHeader cramHeader;
 
 	public CraiIndexer(InputStream is, File output)
 			throws FileNotFoundException, IOException {
 		this.is = new CountingInputStream(is);
-		CramHeader cramHeader = CramIO.readCramHeader(this.is);
-		samFileHeader = cramHeader.samFileHeader;
+		cramHeader = CramIO.readCramHeader(this.is);
 
 		index = new CramIndex(new GZIPOutputStream(new BufferedOutputStream(
 				new FileOutputStream(output))));
@@ -51,7 +48,7 @@ class CraiIndexer {
 
 	private boolean nextContainer() throws IOException {
 		long offset = is.getCount();
-		Container c = CramIO.readContainer(is);
+		Container c = CramIO.readContainer(cramHeader, is);
 		if (c == null)
 			return false;
 		c.offset = offset;
