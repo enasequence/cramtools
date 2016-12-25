d6f04ccf0536d446179451ae83feed13ac98e46b
ALEX VIKHOREV <alv1@mail.ru>
2016-12-25 22:57:10 +0300

    updated AlignmentSliceQuery tests

--- src/test/java/net/sf/cram/TestAlignmentSliceQuery.java
+++ src/test/java/net/sf/cram/TestAlignmentSliceQuery.java
@@ -1,20 +1,43 @@
 package net.sf.cram;
 
 
+import com.sun.org.glassfish.gmbal.Description;
 import htsjdk.samtools.util.StringUtil;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+
+/*
+AlignmentSliceQuery class is designed to parse and print genome location queries of form:
+sequenceName[:start[-end]]
+
+where
+sequenceName:    string matching pattern [A-Za-z0-1_-|]+
+start:    an integer [1, 2^31]
+end:    an integer [1, 2^31]
+
+examples:
+chr1
+X:10000
+ENA|U00096|U00096.3:3000000-3001000
+*/
 
 public class TestAlignmentSliceQuery {
 
     @Test
-	public void testPositive() {
+    public void testPositive() {
         // test positive cases
         doTestPositive("abcd", "", null, "", null);
         doTestPositive("ABCD", "", null, "", null);
 
         doTestPositive("ABCD", ":", 100, "", null);
         doTestPositive("ABCD", ":", 100, "-", 500);
+
+        doTestPositive("chr1", "", null, "", null);
+        doTestPositive("X", ":", 10000, "", null);
+
+        doTestPositive("ENA|U00096|U00096.3", ":", 3000000, "-", 3001000);
     }
 
     @Test
@@ -29,36 +52,59 @@ public class TestAlignmentSliceQuery {
     }
 
     @Test(expected=NullPointerException.class)
-    public void testNegative1() {
+    public void testNegative1()  throws NullPointerException{
         // test negative case
         AlignmentSliceQuery asq = new AlignmentSliceQuery(null);
     }
 
     @Test(expected=ArrayIndexOutOfBoundsException.class)
-    public void testNegative2() {
+    public void testNegative2()  throws  ArrayIndexOutOfBoundsException{
         // test negative case
         AlignmentSliceQuery asq = new AlignmentSliceQuery(":-");
     }
 
     @Test(expected=NumberFormatException.class)
-    public void testNegative3() {
+    public void testNegative3()  throws NumberFormatException {
         // test negative case
         AlignmentSliceQuery asq = new AlignmentSliceQuery("ABCD:-1");
     }
 
     @Test(expected=NumberFormatException.class)
-    public void testNegative4() {
+    public void testNegative4()  throws NumberFormatException{
         // test negative case
         // start underflow
         AlignmentSliceQuery asq = new AlignmentSliceQuery("ABCD:-1-100");
     }
 
     @Test(expected=NumberFormatException.class)
-    public void testNegative5() {
+    public void testNegative5() throws NumberFormatException {
         // test negative case
         // end overflow
         AlignmentSliceQuery asq = new AlignmentSliceQuery("ABCD:1-2147483648");
     }
+
+    @Ignore("Broken test - not sure how tested class should handle 0 as start index")
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testNegative6()  throws IndexOutOfBoundsException {
+        // test negative case
+        // start underflow ( not supposed to start with 0 )
+        AlignmentSliceQuery asq = new AlignmentSliceQuery("ABCD:0-100");
+    }
+
+    @Test
+    public void testNegative7() {
+        try {
+            // test negative case
+            // illegal characters
+            AlignmentSliceQuery asq = new AlignmentSliceQuery("!@#$%");
+
+        } catch (IllegalArgumentException e) {
+            return;
+        }catch(Exception all){}
+
+        Assert.fail("Alignment should not contain illegal characters outside of [A-Za-z0-1_-|]+ range");
+    }
+
 
     private void doTestPositive(String sequence, String sep1, Integer start, String sep2, Integer end)
     {
@@ -67,7 +113,7 @@ public class TestAlignmentSliceQuery {
         Assert.assertNotNull(sep2);
 
         Assert.assertFalse(
-                "Broken Test - 1sr parameter should not contain ':'",
+                "Broken Test - 1st parameter should not contain ':'",
                 sequence.contains(":"));
 
         String testStr =
